0965cf57441800b8c9942df88a66bdf49d7972cd
Krzysztof Kruk <krzysztof.kruk@g.pl>
2020-03-08 13:55:34 +0100

    chapter 6

--- src/app/products/state/products.reducer.ts
+++ src/app/products/state/products.reducer.ts
@@ -1,5 +1,6 @@
 import { Product } from '../product';
 import * as fromRoot from '../../state/app.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 export interface AppState extends fromRoot.AppState {
   products: ProductState;
@@ -8,14 +9,26 @@ export interface AppState extends fromRoot.AppState {
 export interface ProductState {
   showProductCode: boolean;
   currentProduct: Product;
+  currentProductId: number,
   products: Product[];
 }
 
 const initialState: ProductState = {
   showProductCode: true,
   currentProduct: null,
+  currentProductId: null,
   products: []
 }
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(getProductFeatureState, productState => productState.showProductCode);
+export const getCurrentProduct = createSelector(getProductFeatureState, productState => productState.currentProduct);
+export const getCurrentProductId = createSelector(getProductFeatureState, productState => productState.currentProductId);
+export const getProducts = createSelector(getProductFeatureState, productState => productState.products);
+export const getCurrentProductById = createSelector(getProducts, getCurrentProductId, (products, currentProductId) => {
+  return products.find(p => p.id === currentProductId);
+});
 
 export function reducer(state: ProductState = initialState, action): ProductState {
   switch (action.type) {
